2b3cf3a0c1edbf6b36cce3a1cdf30832b70df0b9
Abu Raihan <aburaihanrahmani@gmail.com>
2025-06-18 02:20:03 -0700

    Set response headers

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -1,7 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Welcome to ToDo-App server");
+  if (req.url === "/todos" && req.method === "GET") {
+    res.writeHead(200, {
+      "content-type": "text/plain",
+      //   email: "r@gmail.com",
+    });
+    // res.statusCode = 201;
+    // res.setHeader("content-type", "text/plain");
+    // res.setHeader("email", "r@gmail.com");
+    res.end("Welcome to ToDo-App server");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("Todo created");
+  } else {
+    res.end("Route not pound");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
