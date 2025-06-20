800e84b96620ef8ef84801443b7a6c2b2a57e3f2
Abu Raihan <aburaihanrahmani@gmail.com>
2025-06-20 03:38:35 -0700

    Understanding Middleware in Express

--- module-3/src/app.ts
+++ module-3/src/app.ts
@@ -1,13 +1,20 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { todosRouter } from "./app/todos/todos.routes";
 const app: Application = express();
 
 app.use(express.json());
 app.use("/todos", todosRouter);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to todo app");
-});
+app.get(
+  "/",
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log("Allah");
+    next();
+  },
+  (req: Request, res: Response) => {
+    res.send("Welcome to todo app");
+  }
+);
 // app.get("/todos", );
 
 export default app;
